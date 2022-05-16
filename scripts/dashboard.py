ed7c933a55cfc977256b50a0a29da66153ca14bc
Meron Abate <meriab1234@gmail.com>
2022-05-16 10:27:30 +0300

    data visualaiztion with dashnoard

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -2,17 +2,24 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-
+# Add title
 st.title("TellCo user data analysis")
-st.subheader("user overview analysis")
 
-def main():
+# Add side bar
+st.sidebar.selectbox(
+    "User Analysis", ['User overview analysis', 'Engagment analysis', 'Satisfaction analysis'])
+# configuration
+st.set_option('deprecation.showfileUploaderEncoding', False)
+# # Upload csv file
+# uploaded_file = st.sidebar.file_uploader(
+#     label="Upload your clean data csv file", type=['csv', 'xlsx'])
 
-    df = pd.read_csv('../data/')
+# if uploaded_file is not None:
+#     print(uploaded_file)
+# else:
+#     print("hel")
 
-    page = st.sidebar.selectbox("Choose a page", ['Homepage', 'Exploration', 'Prediction'])
+df = pd.read_csv('../data/analysis_data.csv')
+st.write(df)
+cdf = pd.read_csv('../data/clean_TellCo_data.csv')
+st.write(cdf)
